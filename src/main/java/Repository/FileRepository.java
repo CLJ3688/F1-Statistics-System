67aa5ad73126f0b0238c370896439c2acde0b31a
CLJ3688 <sany983518@gmail.com>
2026-02-08 18:39:57 +0800

    feat: implement multi-source data loading in FileRepository

--- src/main/java/Repository/FileRepository.java
+++ src/main/java/Repository/FileRepository.java
@@ -1,6 +1,9 @@
 package Repository;
 
 import model.Driver;
+import model.RaceDetails;
+import model.RaceResult;
+import model.Team;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,24 +16,92 @@ public class FileRepository {
 
     private final String DATA_PATH = "data";
 
-    public List<Driver> get2024Drivers(String fileName) {
+    public List<Driver> getAllDrivers(String fileName) {
         //get file path
         Path path = Paths.get(DATA_PATH ,fileName);
 
         //try-with-resources
         try (Stream<String> lines = Files.lines(path)){
             return lines
-                    .skip(1) //skip 標題列 (id, code, forename, surname, dob, nationality, team)
+                    .skip(1) //skip 標題列 (driverId, code, forename, surname, dob, nationality)
                     .map(line -> line.split(","))
-                    .filter(parts -> parts.length >= 7) //確保無空行或格式錯誤
+                    .filter(parts -> parts.length >= 6) //確保無空行或格式錯誤
                     .map(parts -> new Driver(
-                            parts[0].trim(), //id
-                            (parts[2].trim() +" " + parts[3].trim()),
-                            parts[6].trim()
+                            parts[0].trim(), //driverId
+                            (parts[2].trim() +" " + parts[3].trim())//name
                     ))
                     .toList();
         } catch (IOException e) {
-            System.err.println("讀取 Driver 檔案發生錯誤 : " + e.getMessage());
+            System.err.println("讀取 " + fileName + " 發生錯誤 : " + e.getMessage());
+            return List.of();
+        }
+    }
+
+    public List<Team> getAllTeams(String fileName) {
+        Path path = Paths.get(DATA_PATH, fileName);
+
+        try(Stream<String> lines = Files.lines(path)) {
+            return lines
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .filter(parts -> parts.length >=3)
+                    .map(parts -> new Team(
+                            parts[0].trim(),
+                            parts[1].trim(),
+                            parts[2].trim()
+                    ))
+                    .toList();
+        } catch (IOException e) {
+            System.err.println("讀取 " + fileName + " 發生錯誤 : " + e.getMessage());
+            return List.of();
+        }
+    }
+
+    public List<RaceResult> getAllRaceResult(String fileName) {
+        Path path = Paths.get(DATA_PATH,fileName);
+
+        try(Stream<String> lines = Files.lines(path)) {
+            return lines
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .filter(parts -> parts.length >= 9)
+                    .map(parts -> new RaceResult(
+                            parts[0].trim(), //resultId
+                            parts[1].trim(),//raceId
+                            parts[2].trim(),//teamId
+                            parts[3].trim(),//driverId
+                            parts[4].trim(),//grid
+                            parts[5].trim(),//position
+                            Integer.parseInt(parts[6].trim()),//points(int)
+                            parts[7].trim(),//year
+                            parts[8].trim()//round
+                    ))
+                    .toList();
+        } catch (IOException e) {
+            System.err.println("讀取 " + fileName + " 發生錯誤 : "  + e.getMessage());
+            return List.of();
+        }
+    }
+
+    public List<RaceDetails> getAllRaceDetails(String fileName) {
+        Path path = Paths.get(DATA_PATH, fileName);
+
+        try(Stream<String> lines = Files.lines(path)) {
+            return lines
+                    .skip(1)
+                    .map(line -> line.split(","))
+                    .filter(parts -> parts.length >=6)
+                    .map(parts -> new RaceDetails(
+                            parts[0].trim(),//raceId
+                            parts[1].trim(),//raceName
+                            Integer.parseInt(parts[2].trim()),//laps(int)
+                            parts[3].trim(),//circuitName
+                            parts[4].trim(),//location
+                            parts[5].trim()//country
+                    ))
+                    .toList();
+        } catch (IOException e) {
+            System.err.println("讀取 " + fileName + " 發生錯誤 : " + e.getMessage());
             return List.of();
         }
     }
