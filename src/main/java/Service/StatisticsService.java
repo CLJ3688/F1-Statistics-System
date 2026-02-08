3705e98db997f0439f0b46f699be31045108c7fe
CLJ3688 <sany983518@gmail.com>
2026-02-08 18:44:37 +0800

    refactor: update StatisticsService to handle new record based data structures

--- src/main/java/Service/StatisticsService.java
+++ src/main/java/Service/StatisticsService.java
@@ -2,7 +2,9 @@ package Service;
 
 import Repository.FileRepository;
 import model.Driver;
+import model.RaceDetails;
 import model.RaceResult;
+import model.Team;
 
 import java.util.Comparator;
 import java.util.List;
@@ -13,11 +15,15 @@ import java.util.stream.Collectors;
 public class StatisticsService {
     FileRepository fileRepository = new FileRepository();
     //get basic Data Set
-    List<Driver> driverList = fileRepository.get2024Drivers("Driver_Details.csv");
-    List<RaceResult> raceResultList = RaceResult.get2024RaceResults();
+    List<Driver> driverList = fileRepository.getAllDrivers("Driver_Details.csv");
+    List<RaceResult> raceResultList = fileRepository.getAllRaceResult("Race_Results.csv");
+    List<Team> teamList = fileRepository.getAllTeams("Team_Details.csv");
+    List<RaceDetails> raceDetailsList = fileRepository.getAllRaceDetails("Race_Details.csv");
+
+//    List<RaceResult> raceResultList = RaceResult.get2024RaceResults();
     //Get a Map<id, name>
     Map<String, String> idToNameMap = driverList.stream()
-            .collect(Collectors.toMap(Driver::id,Driver::name));
+            .collect(Collectors.toMap(Driver::driverId,Driver::name));
     List<String> podiumPosition = List.of("1","2","3");
 
     public record DriverPodiumStats (
@@ -28,36 +34,47 @@ public class StatisticsService {
             double rate
     ){}
 
+
+
+//    public List<Driver> getAllDrivers() {
+//        return this.driverList.stream()
+//                .sorted(Comparator.comparing(Driver::team)
+//                                .thenComparing(Driver::name))
+//                .toList();
+//    }
     public List<Driver> getAllDrivers() {
         return this.driverList.stream()
-                .sorted(Comparator.comparing(Driver::team)
-                                .thenComparing(Driver::name))
+                .sorted(Comparator.comparing(Driver::driverId))
                 .toList();
     }
 
-    public List<DriverPodiumStats> getDriverPodiumStats(String teamName) {
-        //透過 filter 篩選 driver
-        Set<String> teamDriversId = driverList.stream()
-                .filter(driver -> driver.team().contains(teamName))
-                .map(Driver::id)
-                .collect(Collectors.toSet());
-
-        return teamDriversId.stream()
-                .map(driverId -> {
-                    String driverName = idToNameMap.getOrDefault(driverId, "Unknown");
-
-                    List<RaceResult> driverResult = raceResultList.stream()
-                            .filter(raceResult -> raceResult.id().equals(driverId))
-                            .toList();
-
-                    int totalRace = driverResult.size();
-                    long podiumCount = driverResult.stream()
-                            .filter(raceResult -> podiumPosition.contains(raceResult.position()))
-                            .count();
-                    double podiumRate = (totalRace > 0) ? (double) podiumCount / totalRace * 100 : 0.0;
-                    return new DriverPodiumStats(driverId,driverName,totalRace,podiumCount,podiumRate);
-                })
-                .sorted((driver1, driver2) -> Double.compare(driver2.rate(), driver1.rate()))
-                .toList();
+    public List<RaceResult> getRaceResult() {
+        return this.raceResultList;
     }
+
+//    public List<DriverPodiumStats> getDriverPodiumStats(String teamName) {
+//        //透過 filter 篩選 driver
+//        Set<String> teamDriversId = driverList.stream()
+//                .filter(driver -> driver.team().contains(teamName))
+//                .map(Driver::id)
+//                .collect(Collectors.toSet());
+//
+//        return teamDriversId.stream()
+//                .map(driverId -> {
+//                    String driverName = idToNameMap.getOrDefault(driverId, "Unknown");
+//
+//                    List<RaceResult> driverResult = raceResultList.stream()
+//                            .filter(raceResult -> raceResult.id().equals(driverId))
+//                            .toList();
+//
+//                    int totalRace = driverResult.size();
+//                    long podiumCount = driverResult.stream()
+//                            .filter(raceResult -> podiumPosition.contains(raceResult.position()))
+//                            .count();
+//                    double podiumRate = (totalRace > 0) ? (double) podiumCount / totalRace * 100 : 0.0;
+//                    return new DriverPodiumStats(driverId,driverName,totalRace,podiumCount,podiumRate);
+//                })
+//                .sorted((driver1, driver2) -> Double.compare(driver2.rate(), driver1.rate()))
+//                .toList();
+//    }
 }
